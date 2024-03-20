02984f13aec52d29ebe0770594aa173e977edbc1
Victoria <133873038+Vi-Sad@users.noreply.github.com>
2024-03-20 12:34:43 +0300

    Update urls.py

--- petMarket/petMarket/my_app/urls.py
+++ petMarket/petMarket/my_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, re_path
 import my_app.views as my_app
 
 urlpatterns = [
-    path('my_app/', my_app.index),
-    path('my_app/page_1/', my_app.page_1),
-    path('my_app/page_2/', my_app.page_2),
+    path('', my_app.index),
+    # path('my_app/page_1/', my_app.page_1),
+    # path('my_app/page_2/', my_app.page_2),
+    path('products/<int:number_product>/', my_app.products),
 ]

bb7b924fadd78b90b54a3eedf8da67fc7820a699
Victoria <133873038+Vi-Sad@users.noreply.github.com>
2024-03-20 12:36:44 +0300

    Update views.py

--- petMarket/petMarket/pets/views.py
+++ petMarket/petMarket/pets/views.py
@@ -1,6 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("ура страница появилась")
+    return HttpResponse("Главная Кошки Собаки")
+
+
+# def cats(request):
+#     return HttpResponse("<h2>Кошки<h2>")
+
+
+# def dogs(request):
+#     return HttpResponse("<h2>Собаки<h2>")
+
+
+def categories(request, category_id):
+    return HttpResponse(f"Категории: id={category_id}")
+
+
+def pet(request, pet_slug):
+    if pet_slug in ['cats', 'dogs']:
+        return HttpResponse(pet_slug)
+    return HttpResponseNotFound('Error 404!')
+
+
+def petGET(request):
+    title = request.GET.get('title')
+    return HttpResponse(f'Title: {title}')
+

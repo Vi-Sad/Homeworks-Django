369362db55737b45338d9697c9284247f3b2a85c
Victoria <133873038+Vi-Sad@users.noreply.github.com>
2024-03-20 12:35:13 +0300

    Update views.py

--- petMarket/petMarket/my_app/views.py
+++ petMarket/petMarket/my_app/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Page my_app")
+    return HttpResponse("Main page my_app")
 
 
-def page_1(request):
-    return HttpResponse("Page 1")
+# def page_1(request):
+#     return HttpResponse("Page 1")
 
 
-def page_2(request):
-    return HttpResponse("Page 2")
+# def page_2(request):
+#     return HttpResponse("Page 2")
+
+
+def products(request, number_product):
+    if number_product == 1:
+        return HttpResponse("Product 1. Product description...")
+    elif number_product == 2:
+        return HttpResponse("Product 2. Product description...")
+    else:
+        return HttpResponseNotFound(f'Error 404! Product {number_product} not found.')
+

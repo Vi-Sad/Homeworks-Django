06b591beb1013e76b866383c9a12cfe08fa4f818
Victoria <133873038+Vi-Sad@users.noreply.github.com>
2024-03-22 10:03:21 +0300

    Update urls.py

--- petMarket/petMarket/pets/urls.py
+++ petMarket/petMarket/pets/urls.py
@@ -3,9 +3,7 @@ from django.urls import path, re_path
 import pets.views as pets
 
 urlpatterns = [
-    path('', pets.index),  # http://127.0.0.1:8000/pets/
-    # path('cats/', pets.cats),
-    # path('dogs/', pets.dogs),
+    path('', pets.index, name='main_url'),  # http://127.0.0.1:8000/pets/ == main_url
     path('petGET/', pets.petGET),
     path('<slug:pet_slug>/', pets.pet),
     path('categories/<int:category_id>/', pets.categories),

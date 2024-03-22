003c0ce907b50fb62bf33fa6f64c0ad5d43f179d
Victoria <133873038+Vi-Sad@users.noreply.github.com>
2024-03-22 10:02:38 +0300

    Update views.py

--- petMarket/petMarket/my_app/views.py
+++ petMarket/petMarket/my_app/views.py
@@ -10,19 +10,11 @@ def index(request):
     return HttpResponse("Main page my_app")
 
 
-# def page_1(request):
-#     return HttpResponse("Page 1")
-
-
-# def page_2(request):
-#     return HttpResponse("Page 2")
-
-
 def products(request, number_product):
     if number_product == 1:
-        return HttpResponse("Product 1. Product description...")
+        return render(request, 'page_1.html')
     elif number_product == 2:
         return HttpResponse("Product 2. Product description...")
     else:
-        return HttpResponseNotFound(f'Error 404! Product {number_product} not found.')
+        return render(request, "pageNotFound404.html", status=404)
 

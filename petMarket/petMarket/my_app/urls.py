aa4a3dcb9271e9db7a051fa0011fc527de7b8b88
Victoria <133873038+Vi-Sad@users.noreply.github.com>
2024-03-25 13:38:39 +0300

    Update urls.py

--- petMarket/petMarket/my_app/urls.py
+++ petMarket/petMarket/my_app/urls.py
@@ -4,7 +4,5 @@ import my_app.views as my_app
 
 urlpatterns = [
     path('', my_app.index),
-    # path('my_app/page_1/', my_app.page_1),
-    # path('my_app/page_2/', my_app.page_2),
     path('products/<int:number_product>/', my_app.products),
 ]
